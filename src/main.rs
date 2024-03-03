a65c1de61996c0c95f04fe5a1b3d2364a2aa749c
Alexander Permyakov <pamugk@gmail.com>
2024-03-03 18:03:43 +0500

    Application
    - basic CLI app initialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("No arguments passed. Please pass path to OSM PBF file to be used.");
+        return;
+    }
+
+    let reader = osmpbf::ElementReader::from_path(&args[1]);
+
+    loop {
+        println!("Please choose one of the commands below:\n\texit - Exit the program");
+        let mut input = String::new();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+        match input.as_str() {
+            "exit\n" => {
+                println!("Exiting the program…");
+                break;
+            }
+            _ => println!("Unknown command!"),
+        }
+    }
 }
